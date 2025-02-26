4765ff6378160e94437e334baab5e3a31dfc983c
ImmortalIdiot <max.krasulya@yandex.ru>
2025-02-26 19:00:51 +0300

    feat: add auth module

---
+++ auth/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.immortalidiot.auth"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(project(":core:ui"))
+
+    implementation(libs.voyager.navigator)
+    implementation(libs.voyager.transitions)
+}
