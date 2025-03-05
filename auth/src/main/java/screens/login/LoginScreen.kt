691f84924a7e79e5774419d52ecf000d061bb82a
ImmortalIdiot <max.krasulya@yandex.ru>
2025-03-05 19:47:57 +0300

    refactor: set only portrait orientation on the login screen

--- auth/src/main/java/screens/login/LoginScreen.kt
+++ auth/src/main/java/screens/login/LoginScreen.kt
@@ -1,5 +1,7 @@
 package screens.login
 
+import android.app.Activity
+import android.content.pm.ActivityInfo
 import android.content.res.Configuration
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -19,12 +21,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
@@ -34,12 +36,12 @@ import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import com.immortalidiot.auth.R
-import components.bars.TopErrorSnackbar
 import components.bars.LocalSnackbarHostState
+import components.bars.TopErrorSnackbar
 import components.bars.showMessage
 import org.koin.androidx.compose.koinViewModel
-import screens.signup.SignUpScreen
 import screens.reset_password.ResetPasswordScreen
+import screens.signup.SignUpScreen
 
 internal class LoginScreen(
     private val modifier: Modifier
@@ -58,10 +60,8 @@ private fun LoginScreenComposable(
 ) {
     val navigator = LocalNavigator.currentOrThrow
     val context = LocalContext.current
-    val configuration = LocalConfiguration.current
+    val activity = context as Activity
     val snackbarHostState = LocalSnackbarHostState.current
-
-    val isLandScape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
 
     val uiState by viewModel.uiState.collectAsState()
     val login by viewModel.login.collectAsState()
@@ -77,10 +77,18 @@ private fun LoginScreenComposable(
         }
     }
 
+    DisposableEffect(Unit) {
+        activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        onDispose {
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+        }
+    }
+
     Box(
         modifier = modifier
             .fillMaxSize()
-            .padding(vertical = if (!isLandScape) 64.dp else 0.dp),
+            .padding(vertical = 64.dp),
         contentAlignment = Alignment.Center
     ) {
         TopErrorSnackbar(snackbarHostState = snackbarHostState)
@@ -110,7 +118,7 @@ private fun LoginScreenComposable(
                 singleLine = true,
             )
 
-            Spacer(modifier = Modifier.height(height = if (isLandScape) 8.dp else 16.dp))
+            Spacer(modifier = Modifier.height(height = 16.dp))
 
             OutlinedTextField(
                 modifier = Modifier,
@@ -143,7 +151,7 @@ private fun LoginScreenComposable(
                 maxLines = 1,
                 singleLine = true,
             )
-            Spacer(modifier = Modifier.height(height = if (isLandScape) 0.dp else 16.dp))
+            Spacer(modifier = Modifier.height(height = 16.dp))
             Text(
                 modifier = Modifier
                     .align(Alignment.End)
@@ -175,7 +183,7 @@ private fun LoginScreenComposable(
                     style = MaterialTheme.typography.headlineSmall
                 )
             }
-            Spacer(modifier = Modifier.height(height = if (isLandScape) 0.dp else 8.dp))
+            Spacer(modifier = Modifier.height(height = 8.dp))
 
             Text(
                 modifier = Modifier.clickable { navigator.push(SignUpScreen(modifier = modifier)) },
