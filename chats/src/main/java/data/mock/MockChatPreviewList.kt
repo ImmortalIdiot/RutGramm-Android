801f39dec3b44ee84c76cc3ac6a43262302ea65e
ImmortalIdiot <max.krasulya@yandex.ru>
2025-04-11 05:26:30 +0300

    feat: create mock chat preview data

---
+++ chats/src/main/java/data/mock/MockChatPreviewList.kt
@@ -0,0 +1,92 @@
+package data.mock
+
+import data.model.ChatPreviewModel
+
+internal val chats = listOf(
+    ChatPreviewModel(
+        "ImmortalIdiot",
+        "The last famous words",
+        0,
+        "https://static.wikia.nocookie.net/murder-drones/images/c/cd/VMurderDrones2.png/revision/latest?cb=20241113024022"
+    ),
+
+    ChatPreviewModel(
+        "Glebix",
+        "Short sentence",
+        1,
+        "https://i.pinimg.com/564x/5a/2c/a7/5a2ca7ddaa861395c06246a23a9b58dd.jpg"
+    ),
+
+    ChatPreviewModel(
+        "Alpha",
+        "Say \"Hello, world\" and won't forget to put \";\"",
+    ),
+
+    ChatPreviewModel(
+        "Beta",
+        "It was decided to make mock data to test the ui components",
+        15,
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRDE1RwCg3FxvdwpgN9s3Fo2JYun9-V9cCgw&s"
+    ),
+
+    ChatPreviewModel(
+        "The longest chat header title, consisting of nine words and one eighty four symbols.".uppercase(),
+        "Say hello"
+    ),
+
+    ChatPreviewModel(
+        "One more the longest chat header title. It is really long, isn't it?",
+        "Fruit basket",
+        3,
+        "https://i.pinimg.com/736x/2b/47/18/2b4718827a37b6f11dc82e939984c571.jpg"
+    ),
+
+    ChatPreviewModel(
+        "The quick brown fox jumps over the lazy dog.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et varius augue. Vivamus vel viverra.",
+        0,
+        "https://media.istockphoto.com/id/1066252362/photo/close-red-fox-portrait.jpg?s=612x612&w=0&k=20&c=copB5ueYiea6eokxNQ9tNRGaeePrGc8EsDky1bNFZP8="
+    ),
+
+    ChatPreviewModel(
+        "You can lead a horse to water, but you can't make it drink",
+        "Meaning: you can give a person the opportunity to do something, but it's pointless if they don't want to",
+        6,
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0b7R43kY_EqdIA43xwNNU7VmreqqFgS9YaA&s"
+    ),
+
+    ChatPreviewModel(
+        "dessssss",
+        "я не знаю",
+        5,
+        "https://slm-assets.secondlife.com/assets/20942914/lightbox/31594306_lOGlRBcF_c.jpg?1530105681"
+    ),
+
+    ChatPreviewModel(
+        "Seller",
+        "yes",
+        0,
+        "https://cardboardmaster.ru/1487-large_default/bmw-m3-gtr.jpg"
+    ),
+
+    ChatPreviewModel(
+        "Imperia",
+        "Impossible quest",
+        0,
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPsZT7z8O90AhX1fgsjEkBfBUFd9L8iMxp2g&s"
+    ),
+
+    ChatPreviewModel(
+        "Мой город",
+        "Удивительно, но сегодня никого не убили",
+        22,
+        "https://live.staticflickr.com/8076/8266593889_8401e1905e_b.jpg"
+    ),
+
+    ChatPreviewModel(
+        "News",
+        "Google Cloud announces major computing boost with Ironwood chip, new hypercomputer upgrades",
+        14,
+        "https://i.abcnewsfe.com/a/e274162d-2de0-4937-a82c-32aaa82001de/abc_news_live-abc-ml-250210_1739199021469_hpMain_1x1.jpg"
+    )
+)
---
+++ chats/src/main/java/data/model/ChatPreviewModel.kt
@@ -0,0 +1,11 @@
+package data.model
+
+import androidx.compose.runtime.Immutable
+
+@Immutable
+internal data class ChatPreviewModel(
+    val nickname: String,
+    val lastMessage: String = "",
+    val countUnreadMessages: Int = 0,
+    val avatarUrl: String? = null
+)
