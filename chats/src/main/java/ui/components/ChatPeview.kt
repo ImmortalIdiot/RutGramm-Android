755df434e73eb5f652a652567588b65fc564d1f0
ImmortalIdiot <max.krasulya@yandex.ru>
2025-04-18 02:16:31 +0300

    chore: remove unused import

--- chats/src/main/java/ui/components/ChatPeview.kt
+++ chats/src/main/java/ui/components/ChatPeview.kt
@@ -2,7 +2,6 @@ package ui.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
